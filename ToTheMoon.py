875db6f677662e0b25b6da7cda54015e00fb36bb
KennyBoyo <59184727+KennyBoyo@users.noreply.github.com>
2021-07-03 00:13:33 +1000

    Created function for reading the csv file

--- ToTheMoon.py
+++ ToTheMoon.py
@@ -1,17 +1,45 @@
 #hehe xd
-import numpy
-import pandas
-import matplotlib
+import numpy as np
+import pandas as pd
+#import matplotlib
 import csv
 import talib
 
+
+"""
+Final submission function
+
+
+"""
 def getMyPosition():
   pass
+
+"""
+Function which reads the provided csv file and returns a pandas dataframe containing the data
+Parameters:
+    filename: The name of the file which is read
+Returns:
+    A pandas dataframe containing the data values from the file, each index represents a stock
+"""
+def get_data(filename):
+    with open(filename, 'r') as f:
+        try:
+            array = []
+            reader = csv.reader(f, delimiter = ',')
+            for line in reader:
+                array.append(line[0].split())
+            df = pd.DataFrame(np.array(array), dtype=np.float64)
+            #print(df[0])
+        except Exception as e:
+            print(e)
+            pass
+    print(df[0])
+    return df
 
 
 # Conventional main python script setup, also testing
 def main():
-  pass
+  get_data('prices250.txt')
 
 
 if __name__ == "__main__":
