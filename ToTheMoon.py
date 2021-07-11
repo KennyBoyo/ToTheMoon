f3b4c29a52b4357b945bf481f98f5a708861a7dc
Ben <benjaminvonsnarski@gmail.com>
2021-07-11 16:30:30 +1000

    This algo sucks

--- ToTheMoon.py
+++ ToTheMoon.py
@@ -6,34 +6,34 @@ import csv
 import talib as tal
 
 
-def findDiffAvg(stockHis):
-    totalDiff = 0
-    diffLen = len(stockHis) - 1
-    for day in range(diffLen):
-        totalDiff += (stockHis[day + 1] - stockHis[day])
-    return totalDiff / diffLen
+"""Finds the Schaff trend cycle and returns an indicator"""
+def STC(prices):
+    macd, signal, hist = tal.MACD(prices,
+                                  fastperiod=23,
+                                  slowperiod=50,
+                                  signalperiod=10)
+    k, d = tal.STOCH(high=macd,
+                     low=macd,
+                     close=macd,
+                     fastk_period=10,
+                     slowk_period=10)
+
+    macdValue = macd[-1]
+    kValue = k[-1]
+    dValue = d[-1]
+
+    if kValue == dValue:
+        return 0
+
+    schaff = ((macdValue - kValue) / (dValue - kValue)) / 100
+    return schaff
 
 
-# Parameter format: float[stock][day]
+"""Function called by eval.py. Param: float[stock][day]"""
 def getMyPosition(prcHistSoFar):
     newPos = [0 for _ in range(100)]
     for stock in range(100):
-        stockCurrent = prcHistSoFar[stock][-1]
-        stockAvg = numpy.average(prcHistSoFar[stock])
-        stockDiff = findDiffAvg(prcHistSoFar[stock])
-        if (stockDiff > 0 and stockCurrent - stockAvg > 0 or
-            stockDiff < 0 and stockCurrent - stockAvg < 0):
-            newPos[stock] = stockCurrent ** 2 / stockAvg
-        else:
-            newPos[stock] = stockCurrent
+        stockCurrentPrice = prcHistSoFar[stock][-1]
+        schaff = STC(prcHistSoFar[stock])
+        newPos[stock] = stockCurrentPrice * (1 + schaff)
     return newPos
-
-
-# For testing first occurrence (day 200)
-def main():
-    prcAll = eval.loadPrices(eval.pricesFile)
-    getMyPosition(prcAll)
-
-
-if __name__ == "__main__":
-    main()
